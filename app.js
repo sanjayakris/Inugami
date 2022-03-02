d8e3e87953dbdf0ae559c0c3589b3570f60fc1c5
Kris <77112262+sanjayakris@users.noreply.github.com>
2022-03-02 08:03:21 +0700

    create presence listener

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import { Client, Intents } from "discord.js";
+import { Client } from "discord.js";
 import 'dotenv/config';
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: ["DIRECT_MESSAGES", "GUILD_MESSAGES", "GUILDS", "GUILD_PRESENCES"] });
 
 client.once('ready', () =>{
     console.log("Bot is running...");
@@ -10,6 +10,28 @@ client.on('messageCreate', m =>{
     if (m.content === "hi") {
         return m.reply({content: "hello"});
     }
+});
+
+client.on('presenceUpdate', (oldPresence, newPresence) =>{
+    const guild = client.guilds.cache.get('750736254676762725');
+    const modChannel = guild.channels.cache.get('899579969041223680');
+    if (!newPresence.activities[0]) return;
+    if (newPresence.user.bot) return;
+    if (!oldPresence.activities) {
+        oldPresence.activities = [
+            {
+                name: ""
+            }
+        ];
+    }
+    if (!oldPresence.activities[0]) {
+        oldPresence.activities[0] = {
+            name: ""
+        };
+    }
+    if (oldPresence.activities[0].name === newPresence.activities[0].name) return;
+    console.log(`${newPresence.user.tag} is now playing ${newPresence.activities[0].name} (${newPresence.activities[0].id})`);
+    modChannel.send(`${newPresence.user.tag} is now playing ${newPresence.activities[0].name} (${newPresence.activities[0].id})`);
 });
 
 client.login(process.env.CLIENT_TOKEN);
